283c41577e54c3706a451c4053bde91b24411546
Ådne Skjæveland Jøssing <adskjo@gmail.com>
2025-08-15 12:19:25 +0200

    add new files

--- scripts/33_Model_Parse_XFM/run_tensorboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-tensorboard --logdir amrlib/data/model_parse_xfm/runs/ --host 192.168.0.103 --port 6006
-
--- scripts/50_Build_AMR_View/buildUI.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../..
-pyuic5 amrlib/amr_view/mainwindow.ui -o amrlib/amr_view/Ui_MainWindow.py
